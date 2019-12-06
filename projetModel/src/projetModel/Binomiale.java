ced274b57ec2556ebb3f80a5b0d44987dca2b535
Florian Hyver <florian.hyver@iut-rodez.fr>
2019-12-06 11:59:53 +0100

    finalisation de la loi bonomiale

--- projetModel/src/projetModel/Binomiale.java
+++ projetModel/src/projetModel/Binomiale.java
@@ -11,52 +11,67 @@ package projetModel;
  */
 public class Binomiale extends Loi{
 
-    /**
-     * Simule une loi de Bernouilli
-     * @param p la probabilité de succés
-     * @return une simulation de la loi
-     */
-    public static boolean loiBernouilli(double p) {
-        return Math.random() < p;
-    }
+	/** nb de simulation de bernouilli */
+	private int n;
+	/** la probabilité de succés */
+	private double p;
 
-    /**
-     * Simule une loi Binomiale
-     * @param n nombre de répétition de l'expérience
-     * @param p probabilité de succés
-     * @return une simulation de la loi
-     */
-    public static double loiBinomiale(int n, double p) {
-        int nbVrai = 0;
-        for (int i = 0; i < n; i++) {
-            if(loiBernouilli(p)) {
-                nbVrai++;
-            }
-        }
-        return nbVrai/p;        
-    }
+	/**
+	 * Simule une loi de Bernouilli
+	 * @param p la probabilité de succés
+	 * @return une simulation de la loi
+	 */
+	public static boolean loiBernouilli(double p) throws ErreurParametresLoi {
+		if (p < 0 || p > 1) {
+			throw new ErreurParametresLoi("Bernouilli");
+		}
+		return Math.random() < p;
+	}
 
-    /* (non-Javadoc)
-     * @see projetModel.Loi#simuler()
-     */
-    @Override
-    public double[] simuler() throws ErreurParametresLoi {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/**
+	 * Simule une loi Binomiale
+	 * @param n nombre de répétition de l'expérience
+	 * @param p probabilité de succés
+	 * @return une simulation de la loi
+	 */
+	public Binomiale(int n, double p, int nbSimulations) throws ErreurParametresLoi {
+		if (p < 0 || p > 1 || n < 0) {
+			throw new ErreurParametresLoi("Binomiale");
+		}
+		this.n = n;
+		this.p = p;
+		this.simulations = new double[nbSimulations];
+	}
+
+	/* (non-Javadoc)
+	 * @see projetModel.Loi#simuler()
+	 */
+	@Override
+	public double[] simuler() throws ErreurParametresLoi {
+		int nbVrai;
+		for(int j = 0; j < simulations.length ;j++) {
+			//simulation d'une seule loi binoiale grace a Bernouilli
+			nbVrai = 0;
+			for (int i = 0; i < n; i++) {
+				if(loiBernouilli(p)) {
+					nbVrai++;
+				}
+			}
+			simulations[j] = nbVrai/p;
+		}
+		return this.simulations;
+	}
 
 	@Override
 	public double variance() throws ErreurParametresLoi {
-		// TODO Auto-generated method stub
-		return 0;
+		return n*p*(1-p);
 	}
 
 	@Override
 	public double esperance() throws ErreurParametresLoi {
-		// TODO Auto-generated method stub
-		return 0;
+		return n*p;
 	}
-    
-    
+
+
 
 }
